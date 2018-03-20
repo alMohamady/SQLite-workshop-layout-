c30347e1751e409d41b39d3a0a060ca37498c230
alMohamady <al.Mohamady47@gmail.com>
2018-03-20 07:37:28 +0300

    Initial Commit

---
+++ app/src/main/java/com/masafa_eg/sqliteteast/ContactModel.java
@@ -0,0 +1,31 @@
+package com.masafa_eg.sqliteteast;
+
+public class ContactModel {
+
+    private String ID, firstName, lastName;
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
